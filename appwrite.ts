aa5b8b9d08001ca26bf32be6f0c7212f651014c6
unknown <bendahmani34@gmail.com>
2025-08-04 16:21:43 +0200

    Appwrite env

--- appwrite.ts
+++ appwrite.ts
@@ -1,5 +1,10 @@
 export const appwriteConfig = {
+  // * General
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-  platform: "com.xsemxkx.trail",
-  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+  platform: process.env.EXPO_PUBLIC_APPWRITE_PLATFORM,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+
+  // * Collections
+  userCollectionId: process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_USERS
 }
